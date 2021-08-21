862a47f2f53b09d09c26f523236da54b99b96be6
Canderson2120 <connor.anderson@chicagobooth.edu>
2021-08-21 22:05:45 +0000

    Very Best Debug - 3/4 through

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,16 +1,16 @@
 class VenuesController < ApplicationController
 
   def index
-    matching_venues = venue.all
-    venues = matching_venues.order(:created_at)
+    matching_venues = Venue.all
+    @venues = matching_venues.order(:created_at)
 
     render({ :template => "venue_templates/venue_list.html.erb" })
   end
 
   def show
-    venue_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    venue_id = params.fetch("an_id")
+    matching_venues = Venue.where({ :id => venue_id }).at(0)
+    @the_venue = matching_venues
 
     render({ :template => "venue_templates/details.html.erb" })
   end
@@ -37,8 +37,13 @@ class VenuesController < ApplicationController
     redirect_to("/venues/#{venue.id}")
   end
 
+  def test
+
+    redirect_to("/venues")
+  end
+
   def destroy
-    the_id = params.fetch("venue_id")
+    the_id = params.fetch("id_to_delete")
     matching_venues = Venue.where({ :id => the_id })
     venue = matching_venues
     venue.destroy
